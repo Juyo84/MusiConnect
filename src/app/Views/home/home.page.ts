b63775333670ae34c46af33595cec78c2fbe506b
Juyo84 <juliomeme@hotmail.com>
2024-09-27 20:29:53 -0600

    Agregando formato al starter

--- src/app/Views/home/home.page.ts
+++ src/app/Views/home/home.page.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
-import { HeaderComponent } from '../header/header.component';
+import { IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonContent, HeaderComponent],
+  imports: [
+    IonContent,
+    IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+
+    setInterval(() => this.movimientoImagenes(), 3000);
+    
+  }
   
   ngOnInit() {
   }
+
+  movimientoImagenes(){
+    
+    const imagenIzquierda = document.getElementById('imagenIzquierda');
+    const imagenCentral = document.getElementById('imagenCentral');
+    const imagenCambio = document.getElementById('imagenCambio');
+    const imagenDerecha = document.getElementById('imagenDerecha');
+
+    if(imagenIzquierda && imagenCentral && imagenDerecha && imagenCambio){
+      
+      imagenDerecha.id = "imagenCentral";
+      imagenCentral.id = "imagenIzquierda";
+      imagenIzquierda.id = "imagenCambio";
+      imagenCambio.id = "imagenDerecha";
+
+      imagenCambio.setAttribute("src", "https://ionicframework.com/docs/img/demos/avatar.svg");
+    
+    }
+
+  }
+
 }
